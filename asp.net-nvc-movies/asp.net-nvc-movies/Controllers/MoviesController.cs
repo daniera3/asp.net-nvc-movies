4e7f44c125b0bfc825adda54a1e28b30b960d747
daniera3 <34807820+daniera3@users.noreply.github.com>
2019-01-03 16:08:59 +0200

    late start!!!!

---
+++ asp.net-nvc-movies/asp.net-nvc-movies/Controllers/MoviesController.cs
@@ -0,0 +1,361 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+
+namespace WebApplication1.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        string BaseDirectory = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory);
+        string BDirectory = Path.GetDirectoryName((Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory)));
+
+        // GET: Movies
+        private void getMoviesAndGaners()
+        {
+            List<Movies> Movie=new List<Movies>();
+            List<Ganers> Ganer= new List<Ganers>();
+            string path;
+            string DataBacePath = @"Data Source=(LocalDb)\MSSQLLocalDB;AttachDbFilename=" + BaseDirectory + @"\App_Data\aspnet-WebApplication1-20181211112737.mdf;Initial Catalog=aspnet-WebApplication1-20181211112737;Integrated Security=True";
+            path = BaseDirectory + @"\App_Data\allMovies.json"; 
+            try
+            {
+                try
+                {
+                    using (StreamReader r = new StreamReader(path))
+                    {
+                        string json = r.ReadToEnd();
+                        Movie = JsonConvert.DeserializeObject<List<Movies>>(json);
+                    
+                        TempData["MovieList"] = Movie;
+                    }
+                }
+                
+                catch (Exception)
+                {
+                    Process process = new Process();
+                    process.StartInfo.FileName = BDirectory + @"\wecandothis\candoit\bin\Debug\candoit.exe";
+                    process.StartInfo.Arguments = DataBacePath + " " + path;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.RedirectStandardError = true;
+                    process.Start();
+                    //Read the output (or the error)
+                    string output = process.StandardOutput.ReadToEnd();
+                    Console.WriteLine(output);
+                    string err = process.StandardError.ReadToEnd();
+                    Console.WriteLine(err);
+                    process.WaitForExit();
+
+
+
+
+                    using (StreamReader r = new StreamReader(path))
+                    {
+                        string json = r.ReadToEnd();
+                        Movie = JsonConvert.DeserializeObject<List<Movies>>(json);
+                  
+                        TempData["MovieList"] = Movie;
+                    }
+                }
+                path = BaseDirectory + @"\App_Data\allGaners.json";
+                try
+                {
+                    using (StreamReader r = new StreamReader(path))
+                    {
+                        string json = r.ReadToEnd();
+                        Ganer = JsonConvert.DeserializeObject<List<Ganers>>(json);
+                   
+                        TempData["GanersList"] = Ganer;
+                    }
+                }
+                catch (Exception)
+                {
+                    Process process = new Process();
+                    process.StartInfo.FileName = BDirectory + @"\ShowGaners\ShowGaners\bin\Debug\ShowGaners.exe";
+                    process.StartInfo.Arguments = DataBacePath + " " + path;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.RedirectStandardError = true;
+                    process.Start();
+                    //Read the output (or the error)
+                    string output = process.StandardOutput.ReadToEnd();
+                    Console.WriteLine(output);
+                    string err = process.StandardError.ReadToEnd();
+                    Console.WriteLine(err);
+                    process.WaitForExit();
+
+                    using (StreamReader r = new StreamReader(path))
+                    {
+                        string json = r.ReadToEnd();
+                        Ganer = JsonConvert.DeserializeObject<List<Ganers>>(json);
+                        TempData["GanersList"] = Ganer;
+                    }
+                }
+            }
+            catch (Exception) { }
+        }
+        
+        public ActionResult CMovie(string id)
+        {
+            Movies Cmovie = new Movies();
+            try
+            {
+
+              getMoviesAndGaners();
+              List<Movies> Movie = ((IEnumerable<Movies>)TempData["MovieList"]).Cast<Movies>().ToList();
+
+                foreach (Movies mov in Movie)
+                    {
+                        if (mov.Idmovie.ToString() == id)
+                            Cmovie = mov;
+                    }
+                
+            }
+
+            catch (Exception) { return View(Cmovie); }
+            return View(Cmovie);
+
+        }
+
+       
+        public ActionResult Movie(string proj)
+        {
+            /* ViewBag.a = "https://m.media-amazon.com/images/G/01/imdb/images/certificates/us/r-2493392566._CB484113634_.png";
+             ViewBag.b = "https://m.media-amazon.com/images/M/MV5BMjA1MTIwODY4Nl5BMl5BanBnXkFtZTgwNzkxNDc2NjM@._V1_UX140_CR0,0,140,209_AL_.jpg";
+             ViewBag.d = "https://m.media-amazon.com/images/M/MV5BMjMyNzExNzQ5OV5BMl5BanBnXkFtZTgwNjM2MjIxNjM@._V1_UX140_CR0,0,140,209_AL_.jpg";
+             ViewBag.c = "https://m.media-amazon.com/images/M/MV5BOGQzZDM0NGUtZGE1NS00ZjQwLTk0N2EtMWI0NTgxYTkwYWQ4XkEyXkFqcGdeQXVyNDMzMzI5MjM@._V1_UX140_CR0,0,140,209_AL_.jpg";
+             ViewBag.e = "https://ia.media-imdb.com/images/M/MV5BMzcwMDU4NDAyMl5BMl5BanBnXkFtZTcwODAyODQ5Ng@@._V1._SY340_CR2,3,410,315_.jpg";
+             ViewBag.r = "https://ia.media-imdb.com/images/M/MV5BMTMzMTk0MzIwOV5BMl5BanBnXkFtZTcwMjY0NzYxNw@@._V1._SX250_CR0,0,250,315_.jpg";
+             ViewBag.t = "https://m.media-amazon.com/images/M/MV5BMjMzMzQ0NzI5Nl5BMl5BanBnXkFtZTgwNjc2NTY0NjM@._V1_UY209_CR0,0,140,209_AL_.jpg";
+             ViewBag.y = "https://m.media-amazon.com/images/M/MV5BNjk1Njk3YjctMmMyYS00Y2I4LThhMzktN2U0MTMyZTFlYWQ5XkEyXkFqcGdeQXVyODM2ODEzMDA@._V1_UY209_CR34,0,140,209_AL_.jpg";
+             ViewBag.s = "https://m.media-amazon.com/images/M/MV5BMTc1OTc5NzA4OF5BMl5BanBnXkFtZTgwOTAzMzE2NjM@._V1_UY209_CR0,0,140,209_AL_.jpg";
+             ViewBag.h = "https://m.media-amazon.com/images/M/MV5BNzY1MDA2OTQ0N15BMl5BanBnXkFtZTgwMTkzNjU2NTM@._V1_UX140_CR0,0,140,209_AL_.jpg";
+             ViewBag.f = "https://m.media-amazon.com/images/M/MV5BNTkxNzMyMDMyNF5BMl5BanBnXkFtZTgwNjYwMzgxNjM@._V1_UX140_CR0,0,140,209_AL_.jpg";
+             ViewBag.g = "https://m.media-amazon.com/images/M/MV5BZWVkMzY5NzgtMTdlNS00NjY5LThjOTktZWFkNDU3NmQzMDIwXkEyXkFqcGdeQXVyODk2NDQ3MTA@._V1_UY209_CR0,0,140,209_AL_.jpg";
+             ViewBag.g1 = "https://m.media-amazon.com/images/M/MV5BZjFiMGUzMTAtNDAwMC00ZjRhLTk0OTUtMmJiMzM5ZmVjODQxXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_UY209_CR0,0,140,209_AL_.jpg";
+             ViewBag.g2 = "https://m.media-amazon.com/images/M/MV5BNDg2NjIxMDUyNF5BMl5BanBnXkFtZTgwMzEzNTE1NTM@._V1_UX140_CR0,0,140,209_AL_.jpg";
+             ViewBag.g3 = "https://m.media-amazon.com/images/M/MV5BMTkzMzgzMTc1OF5BMl5BanBnXkFtZTgwNjQ4MzE0NjM@._V1_UY209_CR1,0,140,209_AL_.jpg";
+             ViewBag.g4 = "https://m.media-amazon.com/images/M/MV5BZTI4NTE3ODMtYzBhNi00NmE2LWJjY2QtNTUwY2EzYTAzOWUzXkEyXkFqcGdeQXVyODcyODY1Mzg@._V1_UY209_CR11,0,140,209_AL_.jpg";
+             ViewBag.g5 = "https://m.media-amazon.com/images/M/MV5BMTYyNzEyNDAzOV5BMl5BanBnXkFtZTgwNTk3NDczNjM@._V1_UY209_CR0,0,140,209_AL_.jpg";
+             ViewBag.g6 = "https://m.media-amazon.com/images/M/MV5BYmE5Yjg0MzktYzgzMi00YTFiLWJjYTItY2M5MmI1ODI4MDY3XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX140_CR0,0,140,209_AL_.jpg";
+             ViewBag.g7 = "https://m.media-amazon.com/images/M/MV5BMTcxMjUwNjQ5N15BMl5BanBnXkFtZTgwNjk4MzI4NjM@._V1_UY209_CR0,0,140,209_AL_.jpg";
+             ViewBag.Widows = "https://m.media-amazon.com/images/M/MV5BMjM3ODc5NDEyOF5BMl5BanBnXkFtZTgwMTI4MDcxNjM@._V1_UX140_CR0,0,140,209_AL_.jpg";
+             */
+
+
+            string path = BaseDirectory + @"\App_Data\allMovies.json";
+
+            string DataBacePath = @"Data Source=(LocalDb)\MSSQLLocalDB;AttachDbFilename=" + BaseDirectory + @"\App_Data\aspnet-WebApplication1-20181211112737.mdf;Initial Catalog=aspnet-WebApplication1-20181211112737;Integrated Security=True";
+            try
+            {// string DataBacePath = @"Data Source=(LocalDb)\MSSQLLocalDB;AttachDbFilename=D:\Example-of-Microservices\WebApplication1\WebApplication1\App_Data\aspnet-WebApplication1-20181211112737.mdf;Initial Catalog=aspnet-WebApplication1-20181211112737;Integrated Security=True";
+                if (proj == "java")
+                {
+
+
+                    Process process = new Process();
+                    process.StartInfo.FileName = "java";
+                    process.StartInfo.Arguments = "-jar " + BDirectory + @"\retriver.jar" + " jdbc:sqlserver://localhost:1433;databaseName=dbo;integratedSecurity=true;";
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.RedirectStandardError = true;
+                    process.Start();
+                    //Read the output (or the error)
+
+                    string output = process.StandardOutput.ReadToEnd();
+                    Console.WriteLine(output);
+                    string err = process.StandardError.ReadToEnd();
+                    Console.WriteLine(err);
+                    int a = process.ExitCode;
+                    process.WaitForExit();
+
+                    List<Movies> Movie = JsonConvert.DeserializeObject<List<Movies>>(output);
+                    ViewBag.moviesList = Movie;
+                    TempData["MovieList"] = Movie;
+
+                }
+                else
+                {
+
+                    Process process = new Process();
+                    process.StartInfo.FileName = BDirectory + @"\wecandothis\candoit\bin\Debug\candoit.exe";
+                    process.StartInfo.Arguments = DataBacePath + " " + path;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.RedirectStandardError = true;
+                    process.Start();
+                    //Read the output (or the error)
+                    string output = process.StandardOutput.ReadToEnd();
+                    Console.WriteLine(output);
+                    string err = process.StandardError.ReadToEnd();
+                    Console.WriteLine(err);
+                    process.WaitForExit();
+                }
+                getMoviesAndGaners();
+                ViewBag.moviesList = ((IEnumerable<Movies>)TempData["MovieList"]).Cast<Movies>().ToList();
+            }
+            catch (Exception ex)
+            {
+                string a = ex.Message;
+                return View();
+            }
+            return View();
+
+        }
+
+
+        
+        public ActionResult Top10(string num, string prog)
+        {
+          
+            if (num == null)
+                num = "10";
+
+            string path = BaseDirectory+@"\App_Data\TopMovies.json";
+            string DataBacePath = @"Data Source=(LocalDb)\MSSQLLocalDB;AttachDbFilename=" + BaseDirectory + @"\App_Data\aspnet-WebApplication1-20181211112737.mdf;Initial Catalog=aspnet-WebApplication1-20181211112737;Integrated Security=True";
+            //string DataBacePath = @"Data Source=(LocalDb)\MSSQLLocalDB;AttachDbFilename=D:\Example-of-Microservices\WebApplication1\WebApplication1\App_Data\aspnet-WebApplication1-20181211112737.mdf;Initial Catalog=aspnet-WebApplication1-20181211112737;Integrated Security=True";
+            try
+            {
+                if (prog == "java")
+                {
+
+
+                    Process process = new Process();
+                    process.StartInfo.FileName = "java";
+                    process.StartInfo.Arguments = "-jar " + BDirectory + @"\top.jar" + " jdbc:sqlserver://localhost:1433;databaseName=dbo;integratedSecurity=true;";
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.RedirectStandardError = true;
+                    process.Start();
+                    //Read the output (or the error)
+
+                    string output = process.StandardOutput.ReadToEnd();
+                    Console.WriteLine(output);
+                    string err = process.StandardError.ReadToEnd();
+                    Console.WriteLine(err);
+                    int a = process.ExitCode;
+                    process.WaitForExit();
+                    List<Movies> Movie = JsonConvert.DeserializeObject<List<Movies>>(output);
+                    ViewBag.moviesList = Movie;
+                }
+                else
+                {
+                    Process process = new Process();
+                    process.StartInfo.FileName = BDirectory + @"\TopMovies\TopMovies\bin\\Debug\TopMovies.exe";
+                    process.StartInfo.Arguments = DataBacePath + " " + path + " " + num;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.RedirectStandardError = true;
+                    process.Start();
+                    //Read the output (or the error)
+                    string output = process.StandardOutput.ReadToEnd();
+                    Console.WriteLine(output);
+                    string err = process.StandardError.ReadToEnd();
+                    Console.WriteLine(err);
+                    process.WaitForExit();
+                    using (StreamReader r = new StreamReader(path))
+                    {
+                        string json = r.ReadToEnd();
+                        List<Movies> Movie = JsonConvert.DeserializeObject<List<Movies>>(json);
+                        ViewBag.moviesList = Movie;
+                    }
+
+                }
+                getMoviesAndGaners();
+               
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+            ViewBag.num = num;
+            return View();
+            
+        }
+
+        public ActionResult Genre()
+        {
+
+            string path = BaseDirectory+ @"\App_Data\allGaners.json";
+
+            string DataBacePath = @"Data Source=(LocalDb)\MSSQLLocalDB;AttachDbFilename="+ BaseDirectory+@"\App_Data\aspnet-WebApplication1-20181211112737.mdf;Initial Catalog=aspnet-WebApplication1-20181211112737;Integrated Security=True";
+            try
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = BDirectory + @"\ShowGaners\ShowGaners\bin\Debug\ShowGaners.exe";
+                process.StartInfo.Arguments = DataBacePath + " " + path;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.Start();
+                //Read the output (or the error)
+                string output = process.StandardOutput.ReadToEnd();
+                Console.WriteLine(output);
+                string err = process.StandardError.ReadToEnd();
+                Console.WriteLine(err);
+                process.WaitForExit();
+
+                getMoviesAndGaners();
+                ViewBag.GanersList = ((IEnumerable<Ganers>)TempData["GanersList"]).Cast<Ganers>().ToList();
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+            return View();
+
+        }
+ 
+      
+        public ActionResult MoviesPerGaner(string ganer)
+        {
+            ViewBag.ganer = ganer;
+            string path = BaseDirectory+ @"\App_Data\MGaners.json";
+            string DataBacePath = @"Data Source=(LocalDb)\MSSQLLocalDB;AttachDbFilename=" + BaseDirectory + @"\App_Data\aspnet-WebApplication1-20181211112737.mdf;Initial Catalog=aspnet-WebApplication1-20181211112737;Integrated Security=True";
+            //string DataBacePath = @"Data Source=(LocalDb)\MSSQLLocalDB;AttachDbFilename=D:\Example-of-Microservices\WebApplication1\WebApplication1\App_Data\aspnet-WebApplication1-20181211112737.mdf;Initial Catalog=aspnet-WebApplication1-20181211112737;Integrated Security=True";
+            try
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = BDirectory + @"\MoviesPerGaner\MoviesPerGaner\bin\Debug\MoviesPerGaner.exe";
+                process.StartInfo.Arguments = DataBacePath + " " + path + " " + ganer;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.Start();
+                //Read the output (or the error)
+                string output = process.StandardOutput.ReadToEnd();
+                Console.WriteLine(output);
+                string err = process.StandardError.ReadToEnd();
+                Console.WriteLine(err);
+                process.WaitForExit();
+
+
+
+
+
+                using (StreamReader r = new StreamReader(path))
+                {
+                    string json = r.ReadToEnd();
+                    List<Movies> Movie = JsonConvert.DeserializeObject<List<Movies>>(json);
+                    ViewBag.moviesList = Movie;
+                }
+                getMoviesAndGaners();
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+            return View();
+        }
+    }
+
+}
---
+++ asp.net-nvc-movies/asp.net-nvc-movies/Models/Movies.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Movies
+    {
+        public Movies()
+        {
+            this.Str = new List<Star>();
+            this.Dir = new List<Director>();
+            this.Ganer = new List<Ganers>();
+            this.Img = new Imge();
+        }
+        public Movies(int idmovie, string t, int idimg, string time, int rating, string Certificate, string outline)
+        {
+            this.Idmovie = idmovie;
+            this.Title = t;
+            this.Idimg = idimg;
+            this.Time = time;
+            this.Rating = rating;
+            this.Outline = outline;
+            this.Certificate = Certificate;
+
+            this.Str = new List<Star>();
+            this.Dir = new List<Director>();
+            this.Ganer = new List<Ganers>();
+        }
+        public int TOP { get; set; }
+        public Imge Img { get; set; }
+        public string Outline { get; set; }
+        public int Idmovie { get; set; }
+        public string Title { get; set; }
+        public int Idimg { get; set; }
+        public string Time { get; set; }
+        public int Rating { get; set; }
+        public string Certificate { get; set; }
+
+        public List<Star> Str { get; set; }
+        public List<Ganers> Ganer { get; set; }
+        public List<Director> Dir { get; set; }
+
+    }
+
+    public class Director
+    {
+        public int idmovie { get; set; }
+        public string NameDirector { get; set; }
+        public void printAll()
+        {
+            Console.Write(NameDirector + " ");
+
+        }
+        public Director(string url, int id)
+        {
+            idmovie = id;
+            NameDirector = url;
+        }
+    }
+
+    public class Ganers
+    {
+        public int Idmovie { get; set; }
+        public string NameGaner { get; set; }
+        public List<NameMoviesAndIMG> NMAndIMG { get; set; }
+        public Ganers() { }
+        public Ganers(string url, int id)
+        {
+            NMAndIMG = new List<NameMoviesAndIMG>();
+            Idmovie = id;
+            NameGaner = url;
+        }
+    }
+    public class NameMoviesAndIMG
+    {
+        public NameMoviesAndIMG(int a, string b, string c) { Idmovie = a; NameMovies = b; NameIMG = c; }
+        public string NameMovies { get; set; }
+        public string NameIMG { get; set; }
+        public int Idmovie { get; set; }
+    }
+
+    public class Star
+    {
+        public int Idmovie { get; set; }
+        public string Namestar { get; set; }
+        public void PrintAll()
+        {
+            Console.Write(Namestar + " ");
+
+        }
+        public Star(string url, int id)
+        {
+            Idmovie = id;
+            Namestar = url;
+        }
+    }
+
+
+    public class Imge
+    {
+        public int idimg { get; set; }
+        public string img { get; set; }
+        public string title { get; set; }
+        public string alt { get; set; }
+        public string width { get; set; }
+        public string height { get; set; }
+        public void printAll()
+        {
+            Console.Write(img + " " + title + " " + alt + " " + width + " " + height + " ");
+
+        }
+        public Imge() { }
+        public Imge(int id, string url, string titl, string alt, string width, string height)
+        {
+            idimg = id;
+            img = url;
+            title = titl;
+            this.alt = alt;
+            this.width = width;
+            this.height = height;
+        }
+    }
+}
