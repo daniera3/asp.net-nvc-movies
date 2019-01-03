ef3dbe1f1b892d5bc0687fbaeb82a4e47e92095a
daniera3 <34807820+daniera3@users.noreply.github.com>
2019-01-03 16:00:22 +0200

    creat now project

---
+++ asp.net-nvc-movies/asp.net-nvc-movies/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="asp.net_nvc_movies.MvcApplication" Language="C#" %>
---
+++ asp.net-nvc-movies/asp.net-nvc-movies/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(asp.net_nvc_movies.Startup))]
+namespace asp.net_nvc_movies
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
