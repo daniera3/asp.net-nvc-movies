b4c8b14f852b92d86d2f2658a71109559f865b0e
daniera3 <34807820+daniera3@users.noreply.github.com>
2019-01-04 22:39:56 +0200

    creat Manenger

---
+++ asp.net-nvc-movies/asp.net-nvc-movies/Controllers/ManengerController.cs
@@ -0,0 +1,33 @@
+﻿using asp.net_nvc_movies.DEL;
+using asp.net_nvc_movies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace asp.net_nvc_movies.Controllers
+{
+    public class ManengerController : Controller
+    {
+        // GET: ADMIN
+        public ActionResult AddStars()
+        {
+            return View();
+        }
+
+        public ActionResult AddNewMovie(Movies movie)
+        {
+            if (ModelState.IsValid)
+            {
+                MoviesDal dal = new MoviesDal();
+                dal.Movie.Add(movie);
+                dal.SaveChanges();
+                return View(movie);
+            }
+            else
+                return View(movie);
+
+        }
+    }
+}
