4ff5c1fc3f7d4756695c99e057425d53f4cdffd3
daniera3 <34807820+daniera3@users.noreply.github.com>
2019-01-10 10:58:58 +0200

    1

--- project/project/App_Start/RouteConfig.cs
+++ project/project/App_Start/RouteConfig.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using System.Web.Security;
+
 
 namespace project
 {
@@ -12,7 +12,7 @@ namespace project
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            FormsAuthentication.SignOut();
+            
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
